8fe337dc9897c2ff21ba15aa43702e6a0d8e01e2
Lyndsy Simon <lyndsy@lyndsysimon.com>
2014-11-18 19:12:31 -0500

    Add nested channels

--- tests/test_messaging.py
+++ tests/test_messaging.py
@@ -5,16 +5,64 @@ from website.messaging.model import Channel, Message
 from tests.base import DbTestCase
 
 
-class TestMessages(DbTestCase):
+class TestChannels(DbTestCase):
     def test_message_in_channel(self):
         ch = Channel(name="MyChannel")
         ch.save()
 
-        msg = Message()
-        msg.channels = [ch, ]
+        msg = Message(channels=[ch, ])
         msg.save()
 
         assert_equal(ch, Channel.find_one())
         assert_equal(msg, Message.find_one())
 
-        assert_equal(list(ch), [msg])
+        assert_equal([msg], list(ch))
+
+
+class TestNestedChannels(DbTestCase):
+    def setUp(self):
+        super(TestNestedChannels, self).setUp()
+        self.grandchild = Channel(name="Bottom")
+        self.grandchild.save()
+        self.child = Channel(name="Middle", subchannels=[self.grandchild])
+        self.child.save()
+        self.parent = Channel(name="Top", subchannels=[self.child])
+        self.parent.save()
+
+    def test_nested_names(self):
+        assert_equal(
+            self.grandchild._get_nested_names(),
+            {self.grandchild.name}
+        )
+        assert_equal(
+            self.child._get_nested_names(),
+            {self.child.name, self.grandchild.name}
+        )
+        assert_equal(
+            self.parent._get_nested_names(),
+            {self.parent.name, self.child.name, self.grandchild.name}
+        )
+
+    def test_messages(self):
+        top_message = Message(channels=[self.parent])
+        top_message.save()
+        middle_message = Message(channels=[self.child])
+        middle_message.save()
+        bottom_message = Message(channels=[self.grandchild])
+        bottom_message.save()
+
+        assert_equal([bottom_message], list(self.grandchild))
+        assert_equal([middle_message, bottom_message], list(self.child))
+        assert_equal([top_message, middle_message, bottom_message], list(self.parent))
+
+    def test_circular_nesting(self):
+        self.grandchild.subchannels.append(self.parent)
+        self.grandchild.save()
+
+        assert_equal(self.grandchild.subchannels, [self.parent.name])
+
+        all_three = {self.parent.name, self.child.name, self.grandchild.name}
+
+        assert_equal(self.parent._get_nested_names(), all_three)
+        assert_equal(self.child._get_nested_names(), all_three)
+        assert_equal(self.grandchild._get_nested_names(), all_three)
