2b85af6b151ce077444e767ad8a29482d7db36f3
Lyndsy Simon <lyndsy@lyndsysimon.com>
2014-11-18 19:12:31 -0500

    Add nested channels

--- website/messaging/model/__init__.py
+++ website/messaging/model/__init__.py
@@ -7,8 +7,34 @@ class Channel(StoredObject):
     """A conceptual queue of messages"""
     name = fields.StringField(primary=True)
 
+    subchannels = fields.ForeignField('channel',
+                                      list=True,
+                                      default=lambda: list())
+
+    def _get_nested_names(self, names=None):
+        # default to empty set
+        if names is None:
+            names = set()
+
+        # handle circular references
+        if self.name in names:
+            return names
+
+        names.add(self.name)
+
+        # recurse for each subchannel
+        for channel in self.subchannels:
+            names = channel._get_nested_names(names)
+
+        return names
+
     def __iter__(self):
-        for message in Message.find(Q('channels', 'eq', self)):
+        query = Q('channels', 'eq', self.name)
+
+        for name in self._get_nested_names():
+            query |= Q('channels', 'eq', name)
+
+        for message in Message.find(query):
             yield message
 
     def __repr__(self):
