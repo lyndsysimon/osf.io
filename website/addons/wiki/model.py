4d24338979aafd05eb61fe25a39c64560acd0aad
Lauren Revere <Lauren@Laurens-MacBook-Pro.local>
2015-06-23 01:02:21 -0400

    Pull from upstream

--- website/addons/wiki/model.py
+++ website/addons/wiki/model.py
@@ -33,7 +33,8 @@ logger = logging.getLogger(__name__)
 class AddonWikiNodeSettings(AddonNodeSettingsBase):
 
     is_publicly_editable = fields.BooleanField(default=False, index=True)
-    # what are these and do i need to use them? GRUMBLE
+    # what are these and why do i need to use them? GRUMBLE
+    #also can only set editing public if project is public already
 
     def set_editing(self, permissions, auth=None):
         """Set the editing permissions for this node.
